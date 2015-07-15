b547c4f66e479e7c1027b72b5a390b4db9c5e155
Tejas Shah <tejas.urwelcome@gmail.com>
2015-07-15 23:20:20 +0530

    Bundle src: download integrated

--- fetchAndParseTar.js
+++ fetchAndParseTar.js
@@ -9,7 +9,7 @@ var IndexedDb = minimongo.IndexedDb;
 /*
  * Get URL contents viz., hostname, pathname, hash, etc
  */
-var getLocation = function(href) {
+var parseURL = function(href) {
   var match = href.match(/^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)(\/[^?#]*)(\?[^#]*|)(#.*|)$/);
   return match && {
     protocol: match[1],
@@ -23,61 +23,83 @@ var getLocation = function(href) {
 }
 
 /*
- * Fetch the tar from the 'href' passed and parse it on the fly
+ * Fetch the tar from the 'href' passed and parse it on-the-fly
  */
-var fetchAndParseTar = function(db, href, callbackGETRequest){
-  var url = getLocation(href); 
-  // Setting the GET request
+var fetchAndParseTar = function(db, href, cbFetchAndParseTar){
+  var url = parseURL(href);
   var options = {
     //host: 'proxy.iiit.ac.in',
     //port: 8080,
+    protocol: url.protocol,
     host: url.host,
     path: url.pathname,
     method: 'GET',
     responseType: 'arraybuffer',  // set 'responseType' to 'arraybuffer' for the XHR response
-    headers: {'password': ''}     // custom headers 
+    headers: {'password': ''}     // custom headers
   };
 
-  // Toggle 'req' definition according to network requirements:
-  // Without Proxy
-  // var req = https.request(options.path, function (res){
-  // Under Proxy
   var req = http.request(options, function(res){
     var tarParser = tar.extract();
 
     res.on('data', function (chunk) {
-      tarParser.write(new Buffer(chunk));   // Transforming the 'arraybuffer' to 'Buffer' for compatibility with the Stream API
+      // Transforming the 'arraybuffer' to 'Buffer' for compatibility with the Stream API
+      tarParser.write(new Buffer(chunk));
     });
 
     res.on('end', tarParser.end.bind(tarParser));
 
     res.on('error', function (error) {
       console.log(error);
+      cbFetchAndParseTar(error);
     });
 
     // For each file entry, do the following
     tarParser.on('entry', function(header, stream, callback) {
-      console.log("File found " + header.name + " of size ~" + Math.round(header.size/1024) + " KB");
+      console.log("File found " + header.name + " of size ~" +
+                  Math.round(header.size/1024) + " KB");
+      var buffer = [];
+      stream.on('data', function(data){
+        buffer.push(data);
+      });
+
       stream.on('end', function() {
-        // Always use upsert for both inserts and modifies
-        db.tcia.upsert({
-          filename: header.name,
-          size: header.size
-        }, function() {
-          console.log("<< EOF >>");
-          callback();
+        buffer = Buffer.concat(buffer);
+        getSeriesUIDLocation(function(chosenEntry) {
+          if (chosenEntry) {
+            var blob = new Blob([buffer], {type: 'application/octet-binary'});
+            downloadFile(chosenEntry, header.name, blob, 
+                function(errDownloadFile) {
+                  if(!errDownloadFile) {
+                    buffer = [];
+                    updateDB(db, header.name, function(errUpdateDB) {
+                      if(!errUpdateDB) {
+                        console.log("<< EOF >>");
+                        callback();
+                      }
+                      //else cbFetchAndParseTar(errUpdateDB);
+                    });
+                  }
+                  else cbFetchAndParseTar(errDownloadFile);                  
+                });
+          }
+          else {
+            cbFetchAndParseTar("Error: seriesUID not present in " +
+                "chrome.storage.local");
+          }
         });
       })
-      stream.resume();
+      //stream.resume();
     })
 
     .on('finish', function(){
-      console.log("All files in the tar parsed successfully! :)");
+      console.log("All files in the tar downloaded successfully! :)");
+      cbFetchAndParseTar(null);
     });
   });
 
   req.on('error', function (e){
     console.log('problem with request: ' + e);
+    cbFetchAndParseTar(e);
   });
 
   req.end();
@@ -86,26 +108,15 @@ var fetchAndParseTar = function(db, href, callbackGETRequest){
 /*
  * Creates the DB, Collection if not created and calls the main function
  */
-var initFunction = function(href){
+var initFunction = function(href, cbInitFunction){
   db = new IndexedDb({namespace: "mydb"}, function() {
     db.addCollection("tcia", function() {
-      fetchAndParseTar(db, href, function() {});
+      fetchAndParseTar(db, href, function(errFetchAndParseTar) {
+        if(!errFetchAndParseTar) cbInitFunction(null);
+        else cbInitFunction(errFetchAndParseTar);
+      });
     });
   }, function(err) { console.log(err); });
 }
 
-/*  For testing the code directly with node (comment db part)
-
-//href = encodeURI('https://public.cancerimagingarchive.net/nbia-download/servlet/DownloadServlet?userId=nbia_guest&includeAnnotation=true&hasAnnotation=true&seriesUid=1.3.6.1.4.1.14519.5.2.1.6279.6001.465203542815096670421396392391&sopUids=');
-href = encodeURI('http://localhost/gsoc15/tarstream');
-fetchAndParseTar(href, function (data){
-  if(data){
-    console.log("Fetched and parsed all the files");
-  }
-  else{
-    console.log("Error in fetching");
-  }
-});
-
-*/
 module.exports = initFunction; // export the module for browserify
